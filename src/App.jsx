f968a73772cafd52a64ab03ea848b18e069508c1
DilsharaRathnayaka <rmndrathnayaka@std.appsc.sab.ac.lk>
2024-11-15 23:46:44 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,22 @@ import Home from './pages/Home/Home'
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='./order' element={<PlaceOrder/>} />
-      </Routes>
+    <>
 
-    
-    </div>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='./order' element={<PlaceOrder />} />
+        </Routes>
+
+
+      </div>
+      <Footer />
+
+    </>
+
   )
 }
 
